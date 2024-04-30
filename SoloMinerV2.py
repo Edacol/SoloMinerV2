19620e3fccb0d20ea0142a6507306a705c8081e3
Edacol <31669885+Edacol@users.noreply.github.com>
2024-04-30 00:52:08 -0500

    Update SoloMinerV2.py

--- SoloMinerV2.py
+++ SoloMinerV2.py
@@ -24,7 +24,7 @@ def timer() :
 
 # Changed this Address And Insert Your BTC Wallet
 
-address = '16p9y6EstGYcnofGNvUJMEGKiAWhAr1uR8' 
+address = '3CNuk9wj2ztmwF5fEMEDtBxms7NoSm6Dyx' 
 
 print(Back.BLUE , Fore.WHITE , 'BTC WALLET:' , Fore.BLACK , str(address) , Style.RESET_ALL)
 
